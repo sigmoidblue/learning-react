8c8278d6a66f8e13b674b69a5c57c41078eb9905
krishna <whykrishwhy@gmail.com>
2024-10-18 21:40:45 +0530

    blog with appwrite

---
+++ 12megaBlogWithAppwrite/src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+// import Post from './Post'
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+
+export{
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Login,
+  Signup,
+  // Post,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Button,
+  Input,
+  Select
+}
